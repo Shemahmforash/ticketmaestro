209826a00af7b1399d60c9eea50dbfb276c5923f
Carlos Rosão <shemahmforash@gmail.com>
2015-07-26 11:21:22 +0100

    Initial structure

---
+++ ticketmaestro/event.py
@@ -0,0 +1,31 @@
+class Event:
+
+    """
+    This class represents an event
+    """
+
+    section = []
+    row = []
+    seat = None
+
+
+def get_event_data():
+    """This should call an external API or
+    some persistent storage, which would give the room information"""
+    pass
+
+
+def book_tickets(section, positions):
+    pass
+
+
+def buy_tickets(section, positions):
+    pass
+
+
+def section_best_tickets(section, howmany):
+    pass
+
+
+def best_tickets(howmany):
+    pass
