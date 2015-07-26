3570ea1e46be4c04ddb349f88bdd32c93c7b21b3
Carlos Rosão <shemahmforash@hotmail.com>
2015-07-26 15:45:16 +0000

    First iterations in finding best seats

--- ticketmaestro/event.py
+++ ticketmaestro/event.py
@@ -1,28 +1,36 @@
+import copy
+
+
 class Event:
 
     """
-    This class represents an event
+    This class represents an event venue
     """
 
     # an array of arrays representing the venue structure
-    venue = None
+    venue = []
 
     # an array of arrays representing the bookings
     bookings = []
 
-    def __init__(self, sections, rows):
-        self.venue = []
+    def __init__(self, sections, rows, seats_per_row):
         for s in range(sections):
-            row = []
+            section = []
             for r in range(rows):
-                row.append(0)
-            pass
+                row = []
+                for s in range(seats_per_row):
+                    row.append(0)
+                section.append(row)
 
-            self.venue.append(row)
-            # make a copy of row and append it to bookings
-            self.bookings.append(list(row))
+            self.venue.append(section)
 
+        # make bookings a copy of venue
+        self.bookings = copy.deepcopy(self.venue)
+
+        print('venue')
         print(self.venue)
+        print('bookings')
+        print(self.bookings)
 
     def _get_event_data(self):
         """This should call an external API or
@@ -30,57 +38,106 @@ class Event:
 
         pass
 
-    def book_tickets(self, section, positions, user_id):
+    def book_tickets(self, section, seats, user_id):
         """
-        User books a set of position in a specific section
+        User books a set of seats in a specific section
+        Seats - a dictionary with row: [positions]
         """
-
-        # remove duplicates
-        positions = set(positions)
 
         if (not self.venue[section]):
             raise Exception('invalid section for venue')
 
-        for position in positions:
-            if (self.venue[section][position] == 1):
-                raise Exception('position already bought!')
+        for row, positions in seats.iteritems():
+            # remove duplicates
+            positions = set(positions)
+            for position in positions:
+                if (self.venue[section][row][position] == 1):
+                    raise Exception('seat already bought!')
 
-            if (self.bookings[section][position] and
-                    self.bookings[section][position] != user_id):
-                raise Exception('position already booked!')
+                if (self.bookings[section][row][position] and
+                        self.bookings[section][row][position] != user_id):
+                    raise Exception('position already booked!')
 
-            self.bookings[section][position] = user_id
+                self.bookings[section][row][position] = user_id
 
         print('venue')
         print(self.venue)
         print('bookings')
         print(self.bookings)
 
-    def buy_tickets(self, section, positions, user_id):
+    def buy_tickets(self, section, seats, user_id):
         """ In order to buy tickets, one must book them first
         """
         if (not self.venue[section]):
             raise Exception('invalid section for venue')
 
-        # remove duplicates
-        positions = set(positions)
+        for row, positions in seats.iteritems():
+            # remove duplicates
+            positions = set(positions)
+            for position in positions:
+                if (self.venue[section][row][position] == 1):
+                    raise Exception('seat already bought!')
 
-        for position in positions:
-            if (self.venue[section][position] == 1):
-                raise Exception('position already bought!')
+                if (self.bookings[section][row][position] and
+                        self.bookings[section][row][position] != user_id):
+                    raise Exception('position already booked by someone else!')
 
-            # if user as already booked the position, ignore
-            if (self.bookings[section][position] != user_id):
-                raise Exception('position already booked by someone else!')
+                # add bought info to venue and remove booked info from bookings
+                self.bookings[section][row][position] = 0
+                self.venue[section][row][position] = 1
 
-            # add bought info to venue and remove booked info from bookings
-            self.venue[section][position] = 1
-            self.bookings[section][position] = 0
-
+        print('venue')
         print(self.venue)
+        print('bookings')
+        print(self.bookings)
 
-    def section_best_tickets(section, howmany):
-        pass
+    def buy_best_seats(self, howmany, user_id):
+        tickets = self._best_seats(howmany)
 
-    def best_tickets(howmany):
-        pass
+        if (not tickets):
+            raise Exception('No %d adjacent tickets found!' % howmany)
+
+        self.book_tickets(tickets['section'], {
+                          tickets['tickets']['row']:
+                          tickets['tickets']['seats']
+                          }, user_id)
+        self.buy_tickets(tickets['section'], {
+                         tickets['tickets']['row']:
+                         tickets['tickets']['seats']
+                         }, user_id)
+
+    def buy_section_best_seats(self, section, howmany, user_id):
+        tickets = self._section_best_seats(section, howmany)
+
+        if (not tickets):
+            raise Exception(
+                'No %d adjacent tickets found in section!' %
+                (howmany, section))
+
+        self.book_tickets(section, {tickets['row']: tickets['seats']}, user_id)
+        self.buy_tickets(section, {tickets['row']: tickets['seats']}, user_id)
+
+    def _section_best_seats(self, section, howmany):
+        if (not self.venue[section]):
+            raise Exception('invalid section for venue')
+
+        # go trough all rows in section and find n consecutive empty seats
+        for row_idx, row in enumerate(self.bookings[section]):
+            idx = 0
+            while idx < len(row):
+                if [0] * howmany == row[idx:idx + howmany]:
+                    return {'row': row_idx, 'seats': range(idx, idx + howmany)}
+
+                idx += 1
+
+        return []
+
+    def _best_seats(self, howmany):
+        for idx, section in enumerate(self.venue):
+
+            tickets = self._section_best_seats(idx, howmany)
+            print('tickets %s , section %s ' % (tickets, idx))
+            if tickets:
+                return {'section': idx, 'tickets': tickets}
+
+        return {}
--- ticketmaestro/test.py
+++ ticketmaestro/test.py
@@ -1,11 +1,18 @@
 import event
-from sets import Set
 
-ev = event.Event(5, 5)
+ev = event.Event(1, 5, 10)
+
 
 user_id = 123
 
-ev.book_tickets(0, Set([0, 2, 3]), user_id)
+ev.buy_best_seats(5, user_id)
+
+#ev.book_tickets(0, {1: [0, 2, 3]}, user_id)
+
+#ev.book_tickets(0, {0: [0]}, user_id)
 
 
-ev.book_tickets(0, Set([4]), user_id)
+#ev.buy_tickets(0, {0: [0]}, user_id)
+#ev.buy_tickets(0, {0: [0]}, user_id)
+
+# ev.buy_tickets(0, Set([4]), user_id)
