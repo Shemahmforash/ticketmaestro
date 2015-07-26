42cbce4b98c47279c2bad39ab31110a5892c1838
Carlos Rosão <shemahmforash@hotmail.com>
2015-07-26 11:46:03 +0000

    Fixed bugs in booking tickets

--- ticketmaestro/event.py
+++ ticketmaestro/event.py
@@ -3,7 +3,12 @@ class Event:
     """
     This class represents an event
     """
+
+    # an array of arrays representing the venue structure
     venue = None
+
+    # an array of arrays representing the bookings
+    bookings = []
 
     def __init__(self, sections, rows):
         self.venue = []
@@ -12,7 +17,10 @@ class Event:
             for r in range(rows):
                 row.append(0)
             pass
+
             self.venue.append(row)
+            # make a copy of row and append it to bookings
+            self.bookings.append(list(row))
 
         print(self.venue)
 
@@ -22,10 +30,14 @@ class Event:
 
         pass
 
-    def book_tickets(self, section, positions):
+    def book_tickets(self, section, positions, user_id):
         """
         User books a set of position in a specific section
         """
+
+        # remove duplicates
+        positions = set(positions)
+
         if (not self.venue[section]):
             raise Exception('invalid section for venue')
 
@@ -33,27 +45,37 @@ class Event:
             if (self.venue[section][position] == 1):
                 raise Exception('position already bought!')
 
-            if (self.venue[section][position] == '-'):
+            if (self.bookings[section][position] and
+                    self.bookings[section][position] != user_id):
                 raise Exception('position already booked!')
 
-            self.venue[section][position] = '-'
+            self.bookings[section][position] = user_id
 
+        print('venue')
         print(self.venue)
+        print('bookings')
+        print(self.bookings)
 
-    def buy_tickets(self, section, positions):
+    def buy_tickets(self, section, positions, user_id):
         """ In order to buy tickets, one must book them first
         """
         if (not self.venue[section]):
             raise Exception('invalid section for venue')
 
+        # remove duplicates
+        positions = set(positions)
+
         for position in positions:
             if (self.venue[section][position] == 1):
                 raise Exception('position already bought!')
 
-            if (self.venue[section][position] != '-'):
-                raise Exception('position already booked!')
+            # if user as already booked the position, ignore
+            if (self.bookings[section][position] != user_id):
+                raise Exception('position already booked by someone else!')
 
-            self.venue[section][position] = '-'
+            # add bought info to venue and remove booked info from bookings
+            self.venue[section][position] = 1
+            self.bookings[section][position] = 0
 
         print(self.venue)
 
--- ticketmaestro/test.py
+++ ticketmaestro/test.py
@@ -3,8 +3,9 @@ from sets import Set
 
 ev = event.Event(5, 5)
 
+user_id = 123
 
-ev.book_tickets(0, Set([0, 2, 3]))
+ev.book_tickets(0, Set([0, 2, 3]), user_id)
 
 
-ev.book_tickets(0, Set([4]))
+ev.book_tickets(0, Set([4]), user_id)
